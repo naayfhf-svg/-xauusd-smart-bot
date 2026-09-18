992955455cf47b7b98e8e5c0b2c44e47950ec599
naayfhf-svg <naayfhf@gmail.com>
2026-09-18 22:52:43 +0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="بوت الذهب XAU/USD",
+    page_icon="🥇",
+    layout="centered"
+)
+
+st.title("🥇 بوت الذهب XAU/USD")
+st.success("النظام جاهز")
+
+st.metric("حالة البوت", "Paper Trading")
+st.metric("الرصيد التجريبي", "$10,000")
+
+st.info("جاري تجهيز محرك تحليل الذهب...")
